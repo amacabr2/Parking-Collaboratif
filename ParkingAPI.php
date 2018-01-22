311ad991545166ab5d2e577f7ece9d1979491d0b
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-01-22 18:52:21 +0100

    Requete pour ajouter un nouveau parking

--- ParkingAPI.php
+++ ParkingAPI.php
@@ -25,7 +25,19 @@ class ParkingAPI {
         $prepare = $this->bdd->prepare("select * from parking where id = :id");
         $prepare->bindParam(':id', $id);
         $prepare->execute();
-        return $prepare->fetch();
+
+        return $prepare->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function postAdd(array $parking) {
+        $prepare = $this->bdd->prepare("INSERT INTO parking (prix_horaire, surveille, souterrain, disponible, adresse) VALUES (:prix_horaire, :surveille, :souterrain, :disponible, :adresse)");
+        $prepare->bindParam(':prix_horaire', $parking['prix_horaire']);
+        $prepare->bindParam(':surveille', $parking['surveille']);
+        $prepare->bindParam(':souterrain', $parking['souterrain']);
+        $prepare->bindParam(':disponible', $parking['disponible']);
+        $prepare->bindParam(':adresse', $parking['adresse']);
+
+        return $prepare->execute();
     }
 }
 
@@ -36,11 +48,28 @@ if (isset($_GET['todo'])) {
 
     if ($todo == "ALL") {
         $players = $parkingAPI->getAll();
-        var_dump($players);
+        echo json_encode($players);
     } else if ($todo == "SELECT") {
         if (isset($_GET['id'])) {
             $player = $parkingAPI->getSelect(htmlspecialchars($_GET['id']));
-            var_dump($player);
+            echo json_encode($player);
         }
+    }
+}
+
+
+if ($inputJson = file_get_contents('php://input')) {
+    $input = json_decode($inputJson, true);
+
+    if ($input['method'] == "POST") {
+        $parking = [
+            'prix_horaire' => (float)htmlspecialchars($input['data']['prix_horaire']),
+            'surveille' => (int)htmlspecialchars($input['data']['surveille']),
+            'souterrain' => (int)htmlspecialchars($input['data']['souterrain']),
+            'disponible' => (int)htmlspecialchars($input['data']['disponible']),
+            'adresse' => htmlspecialchars($input['data']['adresse'])
+        ];
+
+        echo $parkingAPI->postAdd($parking);
     }
 }
